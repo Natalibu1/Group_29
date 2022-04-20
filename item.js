093a455c2e20d426777718d3ee34c04540cd6404
Natalibu1 <103439754+Natalibu1@users.noreply.github.com>
2022-04-20 14:16:53 +0300

    Update item.js

--- item.js
+++ item.js
@@ -2,7 +2,9 @@ let item_1 = 5
 console.log(item_1)
 let item_2 = 3
 console.log(item_2)
+let item_3
 console.log(item_3 = item_1 + item_2)
+let item_4
 console.log(item_4 = "Yolochka")
 console.log(item_3 + item_4)  
 console.log(item_3 * item_4)
@@ -12,7 +14,7 @@ item_6 = 15
 item_6_type = typeof item_6 
 console.log("item_6 == " + item_6, ", item6_type == " + item_6_type)
 let item_7 = "item_6"
-let item_7_type = typeof item_7
+let item_7_type = String(item_7)
 console.log("item_7 == " + item_7, ", item_7_type == " + item_7_type)
 let age_1 = 10 , age_2 = 18, age_3 = 60
 if (age_1 < age_2) {
