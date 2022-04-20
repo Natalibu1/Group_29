cc38eaaabddb89c962ee901aeda150e3c8214470
Natalibu1 <103439754+Natalibu1@users.noreply.github.com>
2022-04-20 14:18:32 +0300

    Update item.js

--- item.js
+++ item.js
@@ -2,9 +2,8 @@ let item_1 = 5
 console.log(item_1)
 let item_2 = 3
 console.log(item_2)
-let item_3
+let item_3. item_4
 console.log(item_3 = item_1 + item_2)
-let item_4
 console.log(item_4 = "Yolochka")
 console.log(item_3 + item_4)  
 console.log(item_3 * item_4)
