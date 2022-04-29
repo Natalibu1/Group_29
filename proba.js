ec8b190f2791c232ceb8170c9e7e0ee7e4698744
Natalibu1 <103439754+Natalibu1@users.noreply.github.com>
2022-04-29 23:28:10 +0300

    Delete proba.js

--- proba.js
+++
@@ -1,6 +0,0 @@
-function findVowels(str) {
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
-    return str.split('').filter(ltr => vowels.includes(ltr)).length;
-    console.log(str) 
-  }
-findVowels('case')  
